ad506b967cbdd83097e034ba99574d3d046e0e83
shubhu625 <74190027+shubhu625@users.noreply.github.com>
2020-11-09 19:41:32 +0530

    Day-01 Assignment

---
+++ Day-1 Assignment.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+Answer-01: RAINBOW
+# In[1]:
+
+
+#Answer-02:
+print("LETS UPGRADE")
+
+
+# In[3]:
+
+
+#Answer-03:
+a=int(input()) #Cost price
+b=int(input()) #Selling price
+if (b>a):
+    print("Profit")
+elif (a>b):
+    print("Loss")
+else:
+    print("Neither")
+
+
+# In[4]:
+
+
+#Answer-04:
+a=int(input()) #Amount in Euros
+b= a*80        #Amount in Rupees
+print(b)
+
+
+# In[ ]:
+
+
+
+
